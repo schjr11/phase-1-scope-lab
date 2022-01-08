1bacf97ff78af300b2f10469ce21b13938ba383f
Scott Hill <scott.c.hill.jr@gmail.com>
2022-01-07 21:30:47 -0600

    commit tree 1446fce0db71e456387269158b55c9a3763922d9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+    var bestCustomer
+
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+    }
+
+        var bestCustomer
+
+        function overwriteBestCustomer() {
+            bestCustomer = 'maybe bob';
+        }
+
+            const leastFavoriteCustomer = 'bobby'
+
+            function changeLeastFavoriteCustomer() {
+                leastFavoriteCustomer = 'scott';
+            }
